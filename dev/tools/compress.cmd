f43b6046f75d6543279b13bf0b749392f868782d
xieguigang <xie.guigang@gcmodeller.org>
2019-06-26 21:49:35 +0800

    config workflow

--- dev/tools/compress.cmd
+++ dev/tools/compress.cmd
@@ -2,9 +2,7 @@
 
 SET tool=closure-compiler-v20181125.jar
 
-java -jar %tool% --js_output_file=../linq.min.js ../linq.js
-java -jar %tool% --js_output_file=../marked.min.js ../marked.js
-java -jar %tool% --js_output_file=../vbcode.min.js ../vbcode.js
-java -jar %tool% --js_output_file=../visualbasic.wasm.min.js ../visualbasic.wasm.js
-java -jar %tool% --js_output_file=../webgl.render.min.js ../webgl.render.js
-java -jar %tool% --js_output_file=../wast.code.min.js ../wast.code.js
+java -jar %tool% --js_output_file=../../lib/linq.min.js ../build/linq.js
+java -jar %tool% --js_output_file=../../lib/marked.min.js ../build/marked.js
+java -jar %tool% --js_output_file=../../lib/vbcode.min.js ../build/vbcode.js
+java -jar %tool% --js_output_file=../../lib/blog.min.js ../build/blog.js
