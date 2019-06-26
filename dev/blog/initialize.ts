3c854102235960ddbc560809fcecb1699f037be0
xieguigang <xie.guigang@gcmodeller.org>
2019-06-26 21:46:38 +0800

    update modules

--- dev/blog/initialize.ts
+++ dev/blog/initialize.ts
@@ -4,82 +4,36 @@ namespace blog {
 
     let config: markedjs.option = markedjs.option.Defaults;
     let vbcodeStyle: vscode.CSS = vscode.VisualStudio;
-    let language: string = lang();
 
     /**
      * A stack for enable back to previous article
     */
     let history: NamedValue<string>[] = [];
 
-    function lang() {
-        let folder = $ts.location.path;
-
-        if (Strings.Empty(folder) || folder == "/") {
-            return "";
-        } else if (folder.charAt(0) == "#") {
-            return "";
-        } else {
-            return folder.split("/")[0];
-        }
-    }
-
-    function getTargetFile(fileName: string = $ts.location.hash(), multipleLanguage: boolean = true): DocumentFullName {
+    function getTargetFile(fileName: string = $ts.location.hash()): string {
         let pathFallback: string;
-        let path: string;
-        let folder: string = lang();
 
-        if (folder == "vbscripts") {
-            if (!Strings.Empty(fileName, true)) {
-                pathFallback = `/vbscripts/docs/${fileName}.md`;
-            } else {
-                fileName = $ts.location.fileName;
-
-                if (fileName == "index.html" || Strings.Empty(fileName, true)) {
-                    // show home page
-                    pathFallback = "/vbscripts/README.md";
-                } else {
-                    return null;
-                }
-            }
-
-            path = pathFallback;
+        if (!Strings.Empty(fileName, true)) {
+            pathFallback = `/articles/${fileName}.md`;
         } else {
-            if (!Strings.Empty(fileName, true)) {
-                pathFallback = `/docs/${fileName}.md`;
-                path = pathFallback;
-
-                if (multipleLanguage && !Strings.Empty(language, true)) {
-                    path = `/docs/${fileName}.${language}.md`;
-                }
-            } else {
-                // show home page
-                pathFallback = "/README.md";
-                path = pathFallback;
-
-                if (multipleLanguage && !Strings.Empty(language, true)) {
-                    path = `/README.${language}.md`;
-                }
-            }
+            // show home page
+            pathFallback = "/README.md";
         }
 
-        return <DocumentFullName>{
-            path: path,
-            pathFallback: pathFallback
-        }
+        return pathFallback;
     }
 
     export function initialize() {
         // initialize styles and events
-        window.onhashchange = app.loadDocument;
+        window.onhashchange = blog.loadDocument;
         config.renderer = new markdown();
         vbcodeStyle.lineHeight = "5px";
-        language = lang();
 
-        app.renderDocument(getTargetFile());
+        blog.renderDocument(getTargetFile());
     }
 
     export function loadDocument() {
-        app.renderDocument(getTargetFile());
+        blog.renderDocument(getTargetFile());
     }
 
     /**
@@ -110,7 +64,7 @@ namespace blog {
                 let warn = $ts("<p>", {
                     style: "color: lightgrey; background-color: yellow;"
                 }).display(`This article is posted ${days} days before, information in this article may be obsolete...`);
-                                
+
                 dateTag.insertAdjacentElement("afterend", warn);
                 dateTag.insertAdjacentElement("afterend", $ts("<br>"));
             }
@@ -124,41 +78,24 @@ namespace blog {
      * 
      * @param ref The document fullname reference or file basename 
     */
-    export function renderDocument(ref: DocumentFullName | string) {
+    export function renderDocument(ref: string) {
         let count: number = 0;
-        let url: DocumentFullName = (function () {
-            if (typeof ref == "string") {
-                return getTargetFile(ref);
-            } else {
-                return ref;
-            }
-        })();
+        let url: string = getTargetFile(ref);
         let renderDocumentInternal = function (markdown: string) {
             let html: string;
 
             if (Strings.Empty(markdown, true)) {
-                // 404的时候返回的是空字符串
-                if (count == 0) {
-                    count++;
-
-                    // request for fallback document path
-                    $ts.getText(url.pathFallback, renderDocumentInternal, {
-                        nullForNotFound: true
-                    });
-                    return;
-                } else {
-                    // 目标文档查找失败
-                    html = `
+                // 目标文档查找失败
+                html = `
 <h1>404 Not Found</h1>
-<p>The requested URL <strong>${url.path}</strong> was not found on this server.</p>`;
-                }
+<p>The requested URL <strong>${url}</strong> was not found on this server.</p>`;
             } else {
                 html = marked(markdown, config);
             }
 
             let date: string = markdown.match(/[<][!][-]{2,}\s+\d+([-]\d+){2}\s+[-]{2,}>/g)[0];
             let time: Date = new Date(Date.parse(date.match(/\d+([-]\d+){2}/g)[0]));
-            let title: string = vanillavb.app.updateArticle(html, time);
+            let title: string = blog.updateArticle(html, time);
             // push stack
             let frame = new NamedValue<string>(title, $ts.location.hash(<Internal.hashArgument>{
                 trimprefix: false
@@ -197,19 +134,8 @@ namespace blog {
         }
 
         // fetch markdown document from server and run renderer
-        $ts.getText(url.path, renderDocumentInternal, {
+        $ts.getText(url, renderDocumentInternal, {
             nullForNotFound: true
         });
-    }
-
-    export interface DocumentFullName {
-        /**
-         * 带有当前的语言翻译的文档的位置
-        */
-        path: string;
-        /**
-         * 如果目标文档查找失败，则使用这个相对应的默认英文的文档的路径
-        */
-        pathFallback: string;
     }
 }
