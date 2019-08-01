3716cfe217cd2c425689f71c9336bed62f5a4c12
xieguigang <xie.guigang@gcmodeller.org>
2019-08-01 20:26:59 +0800

    update sitemap

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 @echo off
 
 "App\sitemap\siteMap.exe" "D:\developer.gcmodeller.org\gcmodeller-hexo\articles"
+
+"E:\repo\xDoc\App\xDoc.exe"  /build.sitemap /wwwroot "D:\developer.gcmodeller.org\gcmodeller-hexo" /host http://developer.gcmodeller.org
