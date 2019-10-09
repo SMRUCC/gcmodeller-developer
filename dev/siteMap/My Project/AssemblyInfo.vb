45caf8564ae77e365747e865e94e7d38951f38ba
xieguigang <xie.guigang@gcmodeller.org>
2019-10-09 20:38:50 +0800

    edit assembly info

--- dev/siteMap/My Project/AssemblyInfo.vb
+++ dev/siteMap/My Project/AssemblyInfo.vb
@@ -10,9 +10,9 @@ Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyTitle("siteMap")>
 <Assembly: AssemblyDescription("")>
-<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyCompany("gcmodeller.org")>
 <Assembly: AssemblyProduct("siteMap")>
-<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyCopyright("Copyright © developer.gcmodeller.org 2019")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("1.0.*")>
+<Assembly: AssemblyFileVersion("1.22.*")>
