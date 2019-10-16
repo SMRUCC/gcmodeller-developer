6117da72e85e12a1ae17e690a75764fe9d0b28f9
xieguigang <xie.guigang@gcmodeller.org>
2019-10-16 20:56:29 +0800

    update date

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 @echo off
 
-"App\sitemap\siteMap.exe" "D:\developer.gcmodeller.org\gcmodeller-hexo\articles" --toc "D:\developer.gcmodeller.org\gcmodeller-hexo\articles\TOC.html"
+"App\sitemap\siteMap.exe" "D:\developer.gcmodeller.org\gcmodeller-hexo\articles" --toc "D:\developer.gcmodeller.org\gcmodeller-hexo\post.html"
 
 "E:\repo\xDoc\App\xDoc.exe"  /build.sitemap /wwwroot "D:\developer.gcmodeller.org\gcmodeller-hexo" /host http://developer.gcmodeller.org
+
+pause
