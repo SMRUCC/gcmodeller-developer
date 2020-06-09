c8365810c157f5c99a5b8a4eb3987c4531c4597c
xieguigang <xie.guigang@gcmodeller.org>
2020-06-09 20:37:09 +0800

    improvements of the database json model

--- dev/siteMap/Articles.vb
+++ dev/siteMap/Articles.vb
@@ -1,22 +1,64 @@
-﻿Imports Microsoft.VisualBasic.Linq
+﻿Imports Microsoft.VisualBasic.FileIO
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Serialization.JSON
-Imports Microsoft.VisualBasic.ValueTypes
 Imports r = System.Text.RegularExpressions.Regex
 
 Module Articles
 
-    Sub scanArticles(root As String)
+    Public Sub scanDb(root As String)
+        Dim volumns As New List(Of String)
+        Dim topics As New Dictionary(Of String, List(Of articleEntry))
+
+        Const repository = ".repository"
+
+        For Each dir As String In root.ListDirectory(SearchOption.SearchTopLevelOnly).Where(Function(a) Not a.BaseName.TextEquals(repository))
+            Dim vol = scanArticles(dir)
+
+            volumns.Add(vol.name)
+
+            For Each topic In vol.topics
+                If Not topics.ContainsKey(topic.Key) Then
+                    topics.Add(topic.Key, New List(Of articleEntry))
+                End If
+
+                topics(topic.Key).AddRange(topic.Value)
+            Next
+
+            Call vol.GetJson.SaveTo($"{root}/{repository}/{vol.name}.json")
+        Next
+
         Call New DbJson With {
-            .lastupdated = Now.UnixTimeStamp,
-            .articles = root.ListFiles("*.md") _
+            .topics = topics.ToDictionary(Function(a) a.Key, Function(a) a.Value.ToArray)
+        }.GetJson _
+         .SaveTo($"{root}/{repository}/index.json")
+    End Sub
+
+    Public Function scanArticles(volumnFolder As String) As VolumnDb
+        Return New VolumnDb With {
+            .articles = volumnFolder.ListFiles("*.md") _
                 .Where(Function(path) Not path.BaseName.TextEquals("readme")) _
                 .Select(Function(path)
-                            Return parseArticle(path, root.GetDirectoryFullPath)
+                            Return parseArticle(path, volumnFolder.GetDirectoryFullPath)
                         End Function) _
-                .ToArray
-        }.GetJson _
-         .SaveTo($"{root}/db.json")
-    End Sub
+                .ToArray,
+            .name = volumnFolder.BaseName,
+            .topics = .articles _
+                .Select(Function(a) a.topics.Select(Function(t) (t, a))) _
+                .IteratesALL _
+                .GroupBy(Function(a) a.t) _
+                .ToDictionary(Function(a) a.Key,
+                              Function(a)
+                                  Return a _
+                                      .Select(Function(r)
+                                                  Return New articleEntry With {
+                                                      .title = r.a.title,
+                                                      .url = r.a.url
+                                                  }
+                                              End Function) _
+                                      .ToArray
+                              End Function)
+        }
+    End Function
 
     Private Function parseArticle(path As String, root As String) As article
         Dim markdown As String = path.ReadAllText
--- dev/siteMap/JSON.vb
+++ dev/siteMap/JSON.vb
@@ -1,12 +1,42 @@
-﻿
+﻿''' <summary>
+''' 总的数据库索引
+''' </summary>
 Public Class DbJson
 
-    Public Property articles As article()
+    ''' <summary>
+    ''' collection of file basename and <see cref="VolumnDb.name"/>
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property volumns As String()
+    ''' <summary>
+    ''' 总的合并在一起的
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property topics As Dictionary(Of String, articleEntry())
     Public Property lastupdated As Long
 
 End Class
 
-Public Class article
+''' <summary>
+''' 一个年份的文件夹就相当于一个分卷
+''' </summary>
+Public Class VolumnDb
+
+    ''' <summary>
+    ''' the json file basename
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property name As String
+    Public Property articles As article()
+    ''' <summary>
+    ''' 当前的这个分卷的归档
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property topics As Dictionary(Of String, articleEntry())
+
+End Class
+
+Public Class articleEntry
 
     Public Property title As String
     ''' <summary>
@@ -14,6 +44,10 @@ Public Class article
     ''' </summary>
     ''' <returns></returns>
     Public Property url As String
+
+End Class
+
+Public Class article : Inherits articleEntry
 
     Public Property topics As String()
     Public Property authors As String()
